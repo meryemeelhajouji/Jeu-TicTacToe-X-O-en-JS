d9ca0f4667eadfc96b6954b46a14ef28d6761c9d
meryemeelhajouji <meryemelhajouji.99@gmail.com>
2022-09-26 13:54:01 +0200

    script

--- script.js
+++ script.js
@@ -1,35 +1,75 @@
-
-let buttons = document.querySelectorAll('.button')
-buttons = Array.from(buttons)
-
+let buttons = document.querySelectorAll('.button');
+buttons= Array.from(buttons);
+let jouer1 =document.getElementById("jouer1");
+let jouer2 =document.getElementById("jouer2");
+let player =document.querySelector(".player");
+let winner =document.querySelector(".winner");
+let currentPlayer = "X";
 letlistWining = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9],
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
     [1,4,7],
     [2,5,8],
-    [3,6,9],
-    [1,5,9],
-    [3,5,7],
-]
+    [0,4,8],
+    [2,4,6]
+];
 
 
+function theWinner(currentPlayer){
+        
+    letlistWining.forEach(function(combination){
+        let check = combination.every(index => buttons[index].innerText == currentPlayer )
+        if(check){
+            if(currentPlayer == "X" ){
+                  
+                      winner.innerText=  `winner ` + " " + localStorage.getItem("player1")
+                 
+                      
+                    
 
-
-let currentPlayer = "X"
-buttons.forEach(function(button){
-    button.addEventListener('click', function(){
-       
-        button.innerText = currentPlayer
-  
-        if(currentPlayer == "X" ){
-           return currentPlayer = "O" 
-            
-        }else{
-            currentPlayer = "X" 
+                   
+            }else{
+                      winner.innerText=  `winner ` + ` ` + localStorage.getItem("player2")
+                   
+            }             
         }
     })
+}
+function game(){
+
+
+buttons.forEach(function(button ){
+    button.addEventListener('click', function(){
+            
+            localStorage.setItem("player1",jouer1.value);
+            localStorage.setItem("player2",jouer2.value);
+            let b=0
+      
+                b = button.innerText = currentPlayer; 
+        
+        
+            if(currentPlayer == "X" ){
+                currentPlayer = "O" ;
+                 player.innerText = localStorage.getItem("player1");
+            }else{
+                currentPlayer = "X"; 
+                player.innerText = localStorage.getItem("player2");
+                 
+            }
+            theWinner(b)
+                
+            
+            
+            
+          
+        
+    })
 })
+  
+}
+game();
 
 
 
@@ -41,28 +81,6 @@ buttons.forEach(function(button){
 
 
 
-
-let buttons1 = document.getElementById("buttons1").value;
-  let buttons2 = document.getElementById("buttons2").value;
-  let buttons3 = document.getElementById("buttons3").value;
-  let buttons4 = document.getElementById("buttons4").value;
-  let buttons5 = document.getElementById("buttons5").value;
-  let buttons6 = document.getElementById("buttons6").value;
-  let buttons7 = document.getElementById("buttons7").value;
-  let buttons8 = document.getElementById("buttons8").value;
-  let buttons9 = document.getElementById("buttons9").value;
-  let won = document.getElementById("won").value;
-
-  function affichage() {
- 
-  if((buttons1 =='x' && buttons2 =='x' && buttons1 =='x')|| (buttons4 =='x' && buttons5 =='x' && buttons6 =='x')||(buttons7 =='x' && buttons8 =='x' && buttons9 =='x')){
-    won.innerText='congratulation playerX';
-    won.innerHTML = "Player 0 won";
-    window.alert('Player X won');
-
-      }
-  }
-  affichage();
 
 
   
